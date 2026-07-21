783505733f3695dcab1b56d7f14ed2f5a26dfa33
Anchal Pandey <pandeyanchal11548@gmail.com>
2026-07-21 11:37:15 +0530

    Added Correlation Analyzer module

--- src/correlation_analyzer.py
+++ src/correlation_analyzer.py
@@ -4,34 +4,44 @@ import matplotlib.pyplot as plt
 # Load dataset
 df = pd.read_csv("data/sample.csv")
 
+print("===== Correlation Analyzer =====\n")
+
 # Select only numeric columns
-num_df = df.select_dtypes(include="number")
+numeric_df = df.select_dtypes(include=["number"])
 
-# Find correlation
-corr = num_df.corr()
+# Generate correlation matrix
+correlation_matrix = numeric_df.corr()
 
-# Print correlation matrix
-print("Correlation Matrix")
-print(corr)
+print("Correlation Matrix:\n")
+print(correlation_matrix)
 
 # Save correlation matrix
-corr.to_csv("reports/correlation_matrix.csv")
+correlation_matrix.to_csv("reports/correlation_matrix.csv")
 
 # Create heatmap
-plt.figure(figsize=(6,5))
+plt.figure(figsize=(8,6))
 
-plt.imshow(corr, cmap="coolwarm")
+plt.imshow(correlation_matrix, cmap="coolwarm")
 
 plt.colorbar()
 
-plt.xticks(range(len(corr.columns)), corr.columns, rotation=45)
-plt.yticks(range(len(corr.columns)), corr.columns)
+plt.xticks(
+    range(len(correlation_matrix.columns)),
+    correlation_matrix.columns,
+    rotation=90
+)
+
+plt.yticks(
+    range(len(correlation_matrix.columns)),
+    correlation_matrix.columns
+)
 
 plt.title("Correlation Heatmap")
+
+plt.tight_layout()
 
 plt.savefig("reports/correlation_heatmap.png")
 
 plt.show()
 
-print("Correlation matrix saved in reports/correlation_matrix.csv")
-print("Heatmap saved in reports/correlation_heatmap.png")
+print("\nFiles saved successfully!")
